2d81569371c62ae48667c257fc2ad3818a3a0d9e
Tobias Møller <tir@bang-olufsen.dk>
2019-03-20 13:19:31 +0100

    Doc for Convert_files

--- logfile_tests/operations_tests/types/convert_files_test.py
+++ logfile_tests/operations_tests/types/convert_files_test.py
@@ -61,6 +61,7 @@ def file_system(tmp_path):
 
 def test_list_item_in_string_match():
     """
+    Testing correct return for list_item_in_string
     """
     test_item = "c:/test/path/file.txt"
     test_arr = [".txt", ".log", ".exe"]
@@ -68,6 +69,7 @@ def test_list_item_in_string_match():
 
 def test_list_item_in_string_no_match():
     """
+    Testing correct return for list_item_in_string
     """
     test_item = "c:/test/path/file.txt"
     test_arr = [".log", ".exe"]
@@ -75,40 +77,46 @@ def test_list_item_in_string_no_match():
 
 def test_list_item_in_string_arg_item_none():
     """
+    Testing correct return for list_item_in_string
     """
     test_arr = [".log", ".exe"]
     assert not ConvertFiles.list_item_in_string(test_arr, None), "Expected not to be found"
 
 def test_list_item_in_string_arg_arr_none():
     """
+    Testing correct return for list_item_in_string
     """
     test_item = "c:/test/path/file.txt"
     assert not ConvertFiles.list_item_in_string(None, test_item), "Expected not to be found"
 
 def test_convert_file(file_system):
     """
+    Testing convert_file can convert file
     """
     filepath = file_system["main"]["file1"].as_posix()
     result = ConvertFiles.convert_file(filepath, ".log")
     main_file1 = file_system["main"]["file1"].parent / (file_system["main"]["file1"].name + ".log")
-    
+
     assert result, "This should be able to run"
     assert main_file1.exists(), "File is not converted correct"
 
 def test_convert_file_do_not_exists():
     """
+    Testing convert_file returns False when file dont exists
     """
     result = ConvertFiles.convert_file("path/invalid/test.txt", ".log")
     assert not result, "Cant convert that do not exists"
 
 def test_convert_file_arg_filepath_none():
     """
+    Testing convert_file returns False arg is none
     """
     result = ConvertFiles.convert_file(None, ".log")
     assert not result, "Cant convert that do not exists"
 
 def test_convert_file_arg_new_extension_none(file_system):
     """
+    Testing convert_file returns False arg is none
     """
     filepath = file_system["main"]["file1"].as_posix()
     result = ConvertFiles.convert_file(filepath, None)
@@ -116,6 +124,7 @@ def test_convert_file_arg_new_extension_none(file_system):
 
 def test_sort_files_match():
     """
+    Testing sort_files work as expected
     """
     test_filter = [".txt", ".exe"]
     test_filepaths = ["c:/test.txt", "c:/test2.exe", "c:/test3.log", "c:/test4.mov"]
@@ -126,6 +135,7 @@ def test_sort_files_match():
 
 def test_sort_files_no_match():
     """
+    Testing sort_files work as expected
     """
     test_filter = [".dox", ".email"]
     test_filepaths = ["c:/test.txt", "c:/test2.exe", "c:/test3.log", "c:/test4.mov"]
@@ -134,6 +144,7 @@ def test_sort_files_no_match():
 
 def test_sort_files_arg_files_none():
     """
+    Testing sort_files work as expected when arg is none
     """
     test_filter = [".dox", ".email"]
     result = ConvertFiles.sort_files(None, test_filter)
@@ -141,6 +152,7 @@ def test_sort_files_arg_files_none():
 
 def test_sort_files_arg_items_none():
     """
+    Testing sort_files work as expected when arg is none
     """
     test_filepaths = ["c:/test.txt", "c:/test2.exe", "c:/test3.log", "c:/test4.mov"]
     result = ConvertFiles.sort_files(test_filepaths, None)
@@ -148,6 +160,7 @@ def test_sort_files_arg_items_none():
 
 def test_run_invalid_workfolder():
     """
+    Testing run returns false when workfolder invalid
     """
     test_workfolder = None
     test_instructions = {
@@ -160,6 +173,7 @@ def test_run_invalid_workfolder():
 
 def test_run_recursive(file_system):
     """
+    Testing run can run recursively
     """
     test_workfolder = file_system["main"]["dir"]
     test_instructions = {
@@ -172,12 +186,13 @@ def test_run_recursive(file_system):
 
     main_file1 = Path(file_system["main"]["file1"].as_posix() + ".log")
     sub_file1 = Path(file_system["sub"]["file1"].as_posix() + ".log")
-    
+
     assert main_file1.exists(), "This should exists"
     assert sub_file1.exists(), "This should exists"
 
 def test_run_not_recursive(file_system):
     """
+    Testing run can run not recursively
     """
     test_workfolder = file_system["main"]["dir"]
     test_instructions = {
@@ -190,12 +205,13 @@ def test_run_not_recursive(file_system):
 
     main_file1 = Path(file_system["main"]["file1"].as_posix() + ".log")
     sub_file1 = Path(file_system["sub"]["file1"].as_posix() + ".log")
-    
+
     assert main_file1.exists(), "This should exists"
     assert not sub_file1.exists(), "This should not exists"
 
 def test_run_instruction_none(file_system):
     """
+    Testing default instructions is used when instructions is none
     """
     test_workfolder = file_system["main"]["dir"]
     test_instructions = None
@@ -211,9 +227,9 @@ def test_run_instruction_none(file_system):
     assert main_file2.exists(), "This should exists"
     assert main_file3.exists(), "This should exists"
 
-
 def test_run_relative_dir(file_system):
     """
+    Testing relative dir is working
     """
     test_workfolder = file_system["main"]["dir"]
     test_instructions = {
@@ -229,6 +245,7 @@ def test_run_relative_dir(file_system):
 
 def test_run_exclude_extension(file_system):
     """
+    Testing exclude extensions is working
     """
     test_workfolder = file_system["main"]["dir"]
     test_instructions = {
@@ -243,6 +260,7 @@ def test_run_exclude_extension(file_system):
 
 def test_run_exclude_files(file_system):
     """
+    Testing exclude files is working
     """
     test_workfolder = file_system["main"]["dir"]
     test_instructions = {
